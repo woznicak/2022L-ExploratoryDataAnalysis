6de01a8bab777fe3150f182cad6336660124d11e
woznicak <kaswoz4@gmail.com>
2022-03-01 16:56:30 +0100

    cleaning lab01 KW

--- Laboratoria/Lab_1.R
+++ Laboratoria/Lab_1.R
@@ -19,45 +19,92 @@
 # 3) Przypomnienie podstaw R.
 
 data(mtcars)
-head(mtcars)
 
+
+head(mtcars)
+head(mtcars, 10)
+tail(mtcars)
+head(mtcars, 10)
+tail(mtcars, 12)
 ?mtcars
 
 
 ## Jak wybieramy wiersze (obserwacje) oraz kolumny (zmienne)?
 
+mtcars[3,]
+mtcars[,4]
+
 ## Pierwszy wiersz, pierwsza kolumna?
+mtcars[1,1]
 
+rownames(mtcars)
+colnames(mtcars)
 ## 10 pierszych wierszy, 2 i 3 kolumna?
+head(mtcars[,2:3],10)
+mtcars[1:10,2:3]
 
+mtcars[1:10, c(2,5)]
 ## Wszytskie wiersze i kolumny w kolejności "am", "wt", "mpg"?
+mtcars[,c("am",  "mpg", "wt")]
 
+mtcars[,"am"]
 ## Jak wybierać jedną kolumnę?
-
+mtcars$am
 ## Pytania
 
 ## 1. Wymiar ramki danych
+dim(mtcars)
+nrow(mtcars)
+ncol(mtcars)
 
 ## 2. Jakie są typy zmiennych?
+class(mtcars)
+typeof(mtcars)
 
+str(mtcars)
+summary(mtcars)
 ## 3. Ile jest unikalnych wartości zmiennej "cyl" i jakie to są wartości?
-
+unique(mtcars$cyl)
 ## 4. Jaka jest średnia wartość zmiennej "drat" dla samochodów o wartości zmiennej "cyl" równej 4?
-
+mean(mtcars[mtcars$cyl == 4, 'drat'])
 
 ## Prosty wykres
 
 ## Zależność "mpg" i "hp" - scatter plot
-
+plot(mtcars$mpg, mtcars$hp)
 
 
 ## Zmienna "cyl" - barplot
+table(mtcars$cyl)
 
+barplot(table(mtcars$cyl))
 
 # 4) Gra proton, należy stworzyć plik R z kodami do rozwiązania gry (do 20 minut).
 install.packages("proton")
 library(proton)
 proton()
+
+log_insecure <- employees[employees$surname == 'Insecure' & employees$name == 'John', 'login']
+proton(action = "login", login=log_insecure)
+
+for(i in 1:length(top1000passwords)){
+  print(i)
+  response <- proton(action = "login", login=log_insecure, password=top1000passwords[i])
+  if(response == 'Success! User is logged in!'){
+    print(top1000passwords[i])
+    break
+  }
+}
+
+
+log_piet <- employees[employees$surname == 'Pietraszko', 'login']
+head(logs)
+str(logs)
+which.max(table(logs[logs$login == log_piet,'host']))
+table(logs[logs$login == log_piet,'host'])[134]
+
+
+proton(action = "server", host = "194.29.178.16")
 
 
 # 5) Umieszczamy na repozytorium rozwiązanie.
